f4c3ee7c994b0fa1b18a48e6ba7dc391e4f49b83
A S U S <A S U S@LAPTOP-C5SA2S1O>
2022-01-01 15:00:28 +0700

    Ini perulangan yaa, jadi tinggal tambahin yg kira-kira mau ditambahin aja

---
+++ src/projectkel4/ProjectKel4.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectkel4;
+
+/**
+ *
+ * @author A S U S
+ */
+public class ProjectKel4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginMenu lm = new LoginMenu();
+        lm.setLocationRelativeTo(null);
+        lm.setVisible(true);
+    }
+    
+}
